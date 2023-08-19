6197b4249c9f81bc9367cf335fc1f6ff75f91744
Islam Nurmukhametov <islam.nurmuhametov@omega-r.com>
2023-08-20 01:01:10 +0300

    completed challenge

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Ислам Нурмухаметов on 18.08.2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+}
